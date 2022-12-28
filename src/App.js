c1091b24bbea90336cd649d407d0d89322658e5e
Siddharth Kalyani <siddharthkalyani@Siddharths-MacBook-Pro.local>
2022-12-28 16:30:18 +0530

    Added Preloader

--- src/App.js
+++ src/App.js
@@ -8,13 +8,26 @@ import { AlertProvider } from "./context/alertContext";
 import Alert from "./components/Alert";
 import About from "./components/About/About";
 import "./css/style.css";
+import Preloader from "./components/Pre";
+import {useEffect , useState} from "react";
 
 function App() {
 
+  const [load, upadateLoad] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      upadateLoad(false);
+    }, 1000);
+
+    return () => clearTimeout(timer);
+  }, []);
 
   return (
+      
       <ChakraProvider>
       <AlertProvider>
+      <Preloader load={load} />
         <main>
           <Header />
           <LandingSection />
