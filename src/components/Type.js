942e03743f807357a45daec578ca862b7a912f0f
Siddharth Kalyani <siddharthkalyani@Siddharths-MacBook-Pro.local>
2022-12-30 23:47:52 +0530

    Updates

--- src/components/Type.js
+++ src/components/Type.js
@@ -8,7 +8,7 @@ function Type() {
         strings: [
           "Developer",
           "MERN Stack Developer",
-          "Open Source Contributor",
+          "Youtuber",
         ],
         autoStart: true,
         loop: true,
